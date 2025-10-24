f04b6fde80575d17a259e04dd55569e884381f83
Davcode1234 <dpietruszewski12@gmail.com>
2025-10-24 11:54:44 +0200

    Style placker card

--- frontend/src/components/Molecules/PlackerCard/PlackerCard.tsx
+++ frontend/src/components/Molecules/PlackerCard/PlackerCard.tsx
@@ -1,10 +1,26 @@
 import { statusNames } from '../../../statuses';
+import CompanyBatch from '../../Atoms/CompanyBatch/CompanyBatch';
 import styles from './PlackerCard.module.css';
 
 function PlackerCard({ task }) {
+  const companyClass = task.client.split(' ').join('');
+
   return (
     <div className={styles.card}>
-      <p>{task.title}</p>
+      <div className={styles.clientInfoWrapper}>
+        <CompanyBatch
+          companyClass={companyClass}
+          isClientPerson={false}
+          isBigger={false}
+        >
+          {task.client}
+        </CompanyBatch>
+
+        <CompanyBatch companyClass={null} isClientPerson isBigger={false}>
+          {task.clientPerson}
+        </CompanyBatch>
+      </div>
+      <p className={styles.title}>{task.title}</p>
       <p className={styles.statusName}>{statusNames[task.status]}</p>
     </div>
   );
