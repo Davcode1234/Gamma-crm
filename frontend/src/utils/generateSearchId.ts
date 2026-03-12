7a20b541efbe89ee5710f4519cf45afcb4d46fac
Davcode1234 <dpietruszewski12@gmail.com>
2026-03-12 15:12:09 +0100

    Randomize search id generator to temporarly fix bug

--- frontend/src/utils/generateSearchId.ts
+++ frontend/src/utils/generateSearchId.ts
@@ -2,14 +2,14 @@ function generateSearchID(numberOfTasks?) {
   const date = new Date();
   const year = date.getFullYear().toString().slice(2);
   const month = String(date.getMonth() + 1).padStart(2, '0');
-  // const day = date.getDate();
-  // const searchID = `${year}${month}${String(numberOfTasks).padStart(
-  //   4,
-  //   '0'
-  // )}${Math.floor(100000 + Math.random() * 900000)}`;
-  const addOne = Number(numberOfTasks) + 1;
+
+  // const addOne = Number(numberOfTasks) + 1;
+  // const searchID = numberOfTasks
+  //   ? `${year}${month}${String(addOne).slice(4, 8)}`
+  //   : `${year}${month}${Math.floor(1000 + Math.random() * 9000)}`;
+
   const searchID = numberOfTasks
-    ? `${year}${month}${String(addOne).slice(4, 8)}`
+    ? `${year}${month}${Math.floor(1000 + Math.random() * 9000)}`
     : `${year}${month}${Math.floor(1000 + Math.random() * 9000)}`;
   return Number(searchID);
 }
